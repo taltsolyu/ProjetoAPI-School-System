c09fb4853849c22c4d4c4db369b2e3be4be544a6
Talita Yuki <talita.avila@aluno.faculdadeimpacta.com.br>
2025-03-19 11:23:19 -0300

    finalizado

--- src/server.py
+++ src/server.py
@@ -7,7 +7,7 @@ dici = {
         {
         'id': 1023,
         'nome': 'Lucas Silva',
-        'idade': 21,
+        'idade': 41,
         'materia': 'Programação Orientada a Objetos',
         'observacoes': 'O professor explica os conteúdos de forma clara e organizada, facilitando o entendimento dos temas abordados.'
         },
@@ -15,15 +15,15 @@ dici = {
         {
         'id': 1024,
         'nome': 'Ana Oliveira',
-        'idade': 22,
+        'idade': 42,
         'materia': 'Banco de Dados',
-        'observacoes': 'Durante as aulas, ele incentiva a participação dos alunos, fazendo perguntas e promovendo discussões para garantir que todos acompanhem o ritmo.'
+        'observacoes': 'Durante as aulas, ela incentiva a participação dos alunos, fazendo perguntas e promovendo discussões para garantir que todos acompanhem o ritmo.'
         },
         
         {
         'id': 1025,
         'nome': 'Pedro Santos',
-        'idade': 20,
+        'idade': 30,
         'materia': 'Estrutura de Dados',
         'observacoes': 'Ele costuma apresentar exemplos do dia a dia ou casos reais para ajudar na aplicação dos conceitos.'
         },
@@ -31,7 +31,7 @@ dici = {
         {
         'id': 1026,
         'nome': 'Mariana Costa',
-        'idade': 23,
+        'idade': 63,
         'materia': 'Redes de Computadores',
         'observacoes': ' A aula é bem estruturada e dinâmica, o que mantém a atenção dos alunos e evita que o conteúdo fique monótono.'
         },
@@ -39,61 +39,63 @@ dici = {
         {
         'id': 1027,
         'nome': 'Carlos Lima',
-        'idade': 24,
+        'idade': 34,
         'materia': 'Desenvolvimento Web',
         'observacoes': 'O professor dá retorno sobre as dúvidas e os exercícios feitos em aula, reforçando os pontos fortes e ajudando a corrigir os erros de forma construtiva.'
         }
         ]
     }
    
-#ler
 @app.route("/professores", methods=["GET"])
 def getTodosProfessors():
   return jsonify(dici["professores"])
 
-#criar
-campos_obrigatorios = ["nome","idade","materia","observacoes"]
-
 @app.route("/professores",methods=["POST"])
 def criarProfessor():
     dados = request.get_json()
-    for campo in campos_obrigatorios:
-        if campo not in dados or not dados[campo]:
-            return jsonify({"mensagem": f"O campo '{campo}' é obrigatório e deve estar preenchido."}), 400
-    return jsonify({"mensagem": "Todos os campos obrigatórios estão preenchidos."}), 200
+    for key, value in dados.items():
+        if(not value):
+          return jsonify({"mensagem": f"O campo '{key}' é obrigatório e deve estar preenchido."}), 400
+    return jsonify(dados)
 
+@app.route("/professores/<int:idProfessor>", methods=['GET'])
+def getByIdProfessor(idProfessor):
+    for professor in dici['professores']:
+        if professor['id'] == idProfessor:
+            return jsonify(professor)
+    return jsonify(None), 204
 
-    
- 
- 
- 
- 
- 
- 
- 
- 
-# #att
-# @app.route("/professores/<int:idProfessor>", methods=['PUT'])
-# def updateProfessores(idProfessor):
-#     professores = dici["professores"]
-#     for professor in professores:
-#         if professor['id'] == idProfessor:
-#             dados = request.json
-#             professor["id"] = dados['id']
-#             professor['nome'] = dados['nome']
-#             return jsonify(dados)
-#         else:
-#             return jsonify("professor nao encontrado")
+@app.route("/professores/<int:idProfessor>", methods=['PUT'])
+def updateProfessor(idProfessor):
+    professor = request.get_json()
+    for i in range (0,len(dici['professores'])):
+        if dici['professores'][i]['id'] == idProfessor:
+            professor_desatualizado = dici['professores'][i] 
+            professor_att = merge_dicts(professor_desatualizado,professor)
+            dici['professores'][i] = professor_att
+            return jsonify(professor_att)
+    return jsonify(None), 400
 
-# #deletar
-# @app.route("/professores/<int:idProfessor>",methods=['DELETE'])
-# def deleteProfessor(idProfessor):
-#     professores = dici["professores"]
-#     for professor in professores:
-#         if professor ['id'] == idProfessor:
-#             professores.remove(professor)
-#             return jsonify({"mensagem": "Professor removido com sucesso"})
-#     return jsonify ({"mensagem": "Professor não encontrado"}), 404
+def merge_dicts(dict1, dict2):
+    merged = dict1.copy()  
+    for key, value in dict2.items():
+        if key in merged:
+            if merged[key] != value:
+                merged[key] = value
+        else:
+            merged[key] = value  
+
+    return merged
+
+@app.route("/professores/<int:idProfessor>", methods=['DELETE'])
+def deletarProfessor(idProfessor):
+    for professor in dici['professores']:
+        if professor['id'] == idProfessor:
+            id_para_remover = idProfessor
+            dici['professores'] = [prof for prof in dici['professores'] if prof['id'] != id_para_remover]
+            return jsonify(professor)
+    return jsonify(None), 400
+
 
 if __name__ == '__main__':
         app.run(host = 'localhost', port = 5000, debug = True)
--- tests/testes.py
+++ tests/testes.py
@@ -6,22 +6,130 @@ class TestTeacherMethods(unittest.TestCase):
   
   def test_000_professores_retorna_lista(self):
     response = requests.get('http://localhost:5000/professores').json()
-    print(response)
     self.assertEqual(type(response), type([]))
     
   def test_001_criar_professor_sucesso(self):
-    professores = {
+    professor = {
       "id": 1028,
-      "nome": "José",
+      "nome": "José Reis",
       "idade": 35,
       "materia": "SQL",
-      "observacoe": "Ele disponibiliza materiais complementares, como slides, artigos e listas de exercícios, que ajudam os alunos a revisar e aprofundar o conteúdo após a aula."
+      "observacoes": "Ele disponibiliza materiais complementares, como slides, artigos e listas de exercícios, que ajudam os alunos a revisar e aprofundar o conteúdo após a aula."
     }
     
-    response = requests.post("http://localhost:5000/professores",json=professores)
+    response = requests.post("http://localhost:5000/professores",json=professor) 
     print(response)
-    self.assertIsNotNone(response.json())
+    self.assertEqual(response.json(), professor)
+   
+  def test_002_criar_professor_erro(self):
+    professor = {
+      "id": None,
+      "nome": "José Reis",
+      "idade": 35,
+      "materia": "SQL",
+      "observacoes": "Ele disponibiliza materiais complementares, como slides, artigos e listas de exercícios, que ajudam os alunos a revisar e aprofundar o conteúdo após a aula."
+    }
+    response = requests.post("http://localhost:5000/professores",json=professor)
+    self.assertEqual(response.status_code,400)
+    response_data = response.json()
+    self.assertEqual(response_data['mensagem'], "O campo 'id' é obrigatório e deve estar preenchido.")
+  
+    professor = {
+      "id": 1502,
+      "nome": None,
+      "idade": 35,
+      "materia": "SQL",
+      "observacoes": "Ele disponibiliza materiais complementares, como slides, artigos e listas de exercícios, que ajudam os alunos a revisar e aprofundar o conteúdo após a aula."
+    }
+    response = requests.post("http://localhost:5000/professores",json=professor)
+    self.assertEqual(response.status_code,400)
+    response_data = response.json()
+    self.assertEqual(response_data['mensagem'], "O campo 'nome' é obrigatório e deve estar preenchido.")
+    professor = {
+      "id": 1502,
+      "nome": "José Reis",
+      "idade": None,
+      "materia": "SQL",
+      "observacoes": "Ele disponibiliza materiais complementares, como slides, artigos e listas de exercícios, que ajudam os alunos a revisar e aprofundar o conteúdo após a aula."
+    }
+    response = requests.post("http://localhost:5000/professores",json=professor)
+    self.assertEqual(response.status_code,400)
+    response_data = response.json()
+    self.assertEqual(response_data['mensagem'], "O campo 'idade' é obrigatório e deve estar preenchido.")
+    professor = {
+      "id": 1502,
+      "nome": "José Reis",
+      "idade": 35,
+      "materia": None,
+      "observacoes": "Ele disponibiliza materiais complementares, como slides, artigos e listas de exercícios, que ajudam os alunos a revisar e aprofundar o conteúdo após a aula."
+    }
+    response = requests.post("http://localhost:5000/professores",json=professor)
+    self.assertEqual(response.status_code,400)
+    response_data = response.json()
+    self.assertEqual(response_data['mensagem'], "O campo 'materia' é obrigatório e deve estar preenchido.")
+    
+  def test_003_buscar_professor_id_sucesso(self):
+    id = 1023
+    response = requests.get(f"http://localhost:5000/professores/{id}")
+    professor = response.json()
+    self.assertEqual(response.status_code, 200)
+    self.assertIsNotNone(professor)
+    self.assertEqual(professor['nome'], "Lucas Silva")
+    
+  def test_004_buscar_professor_id_erro(self):
+    id = 1029
+    response = requests.get(f"http://localhost:5000/professores/{id}")
+    self.assertEqual(response.status_code, 204)
 
+  def test_005_att_professor_sucesso(self):
+    professor_att = {
+      'id': 1024,
+      'nome': 'Ana Oliveira',
+      'idade': 43,
+      'materia': 'Matemática Aplicada',
+      'observacoes': 'Durante as aulas, ela incentiva a participação dos alunos, fazendo perguntas e promovendo discussões para garantir que todos acompanhem o ritmo.'
+    }
+    response = requests.put(f"http://localhost:5000/professores/{professor_att['id']}",json=professor_att)
+    self.assertEqual(response.status_code,200)
+    self.assertEqual(response.json(),professor_att)
+
+  def test_006_att_professor_erro(self):
+    professor_att = {
+      'id': 1400,
+      'nome': 'Ana Oliveira',
+      'idade': 43,
+      'materia': 'Matemática Aplicada',
+      'observacoes': 'Durante as aulas, ela incentiva a participação dos alunos, fazendo perguntas e promovendo discussões para garantir que todos acompanhem o ritmo.'
+    }
+    response = requests.put(f"http://localhost:5000/professores/{professor_att['id']}",json=professor_att)
+    self.assertEqual(response.status_code,400)
+
+  def test_007_deletar_professor_sucesso(self):
+    professor_removido = {
+        'id': 1025,
+        'nome': 'Pedro Santos',
+        'idade': 30,
+        'materia': 'Estrutura de Dados',
+        'observacoes': 'Ele costuma apresentar exemplos do dia a dia ou casos reais para ajudar na aplicação dos conceitos.'
+    }
+    requests.post("http://localhost:5000/professores", json=professor_removido)
+    response = requests.delete(f"http://localhost:5000/professores/{professor_removido['id']}")
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.json(), professor_removido)
+ 
+  def test_008_deletar_professor_erro(self):
+    professor_removido = {
+        'id': 15,
+        'nome': 'Pedro Santos',
+        'idade': 30,
+        'materia': 'Estrutura de Dados',
+        'observacoes': 'Ele costuma apresentar exemplos do dia a dia ou casos reais para ajudar na aplicação dos conceitos.'
+    }
+    requests.post("http://localhost:5000/professores", json=professor_removido)
+    response = requests.delete(f"http://localhost:5000/professores/{professor_removido['id']}")
+    self.assertEqual(response.status_code, 400)
+ 
+ 
 def runTests():
   suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestTeacherMethods)
   unittest.TextTestRunner(verbosity=2,failfast=True).run(suite)
